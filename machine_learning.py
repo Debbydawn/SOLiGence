a57df7e8bba7b65c378737db93892a33b3138ef4
debydawn <debbydawn16@gmail.com>
2023-08-28 15:47:19 +0100

    ARIMA updated

--- machine_learning.py
+++ machine_learning.py
@@ -5,14 +5,13 @@ from statsmodels.tsa.arima.model import ARIMA
 from yahoofinancials import YahooFinancials
 import datetime
 
-
 # List of cryptocurrency symbols
 crypto_symbols = ['BTC-GBP', 'ETH-GBP', 'LTC-GBP']
 
 # Dictionary to store cryptocurrency data
 crypto_data = {}
 
-# Loop through each cryptocurrency symbol
+# Loop through each cryptocurrency symbol to fetch historical data
 for symbol in crypto_symbols:
     yahoo_financials = YahooFinancials(symbol)
     
@@ -22,9 +21,9 @@ for symbol in crypto_symbols:
     # Get historical price data up to the current date
     data = yahoo_financials.get_historical_price_data("2019-07-10", end_date, "daily")
     prices = data[symbol]['prices']
-    df = pd.DataFrame(prices).drop('date', axis=1).set_index('formatted_date')
     
-    # Explicitly set the frequency to 'D' (daily)
+    # Create a DataFrame and set the date index with explicit frequency
+    df = pd.DataFrame(prices).drop('date', axis=1).set_index('formatted_date')
     df.index = pd.DatetimeIndex(df.index, freq='D')
     
     crypto_data[symbol] = df
@@ -33,24 +32,15 @@ for symbol in crypto_symbols:
 order = (5, 1, 0)  # (p, d, q)
 forecast_steps = 30
 
-
-
+# Print the forecasts for each symbol
 for symbol in crypto_symbols:
     crypto_series = crypto_data[symbol]['close']
     arima_model = ARIMA(crypto_series, order=order)
     arima_results = arima_model.fit()
     forecast = arima_results.forecast(steps=forecast_steps)
     
+    print(f"Forecasts for {symbol}:")
     print(forecast)
-    
-    
-    # ... (Previous code)
-
-for symbol in crypto_symbols:
-    crypto_series = crypto_data[symbol]['close']
-    arima_model = ARIMA(crypto_series, order=order)
-    arima_results = arima_model.fit()
-    forecast = arima_results.forecast(steps=forecast_steps)
     
     # Plot the original data and the forecast for each symbol
     plt.figure(figsize=(12, 6))
@@ -69,11 +59,10 @@ for symbol in crypto_symbols:
     plt.grid(True)
     plt.show()
 
-    
-
-# Create a figure and axis for the plot
+# Create a figure and axis for the combined forecast plot
 plt.figure(figsize=(12, 6))
 
+# Plot the forecasts for each symbol together
 for symbol in crypto_symbols:
     crypto_series = crypto_data[symbol]['close']
     arima_model = ARIMA(crypto_series, order=order)
@@ -86,8 +75,26 @@ for symbol in crypto_symbols:
 
 plt.xlabel('Date')
 plt.ylabel('Price')
-plt.title('ARIMA Cryptocurrency Price Forecasts')
+plt.title('Combined ARIMA Cryptocurrency Price Forecasts')
 plt.legend()
 plt.grid(True)
 plt.show()
 
+for symbol in crypto_symbols:
+    crypto_series = crypto_data[symbol]['close']
+    arima_model = ARIMA(crypto_series, order=order)
+    arima_results = arima_model.fit()
+    forecast = arima_results.forecast(steps=forecast_steps)
+    
+    # Plot the forecast for each symbol
+    plt.figure(figsize=(12, 6))
+    
+    forecast_index = pd.date_range(start=crypto_series.index[-1], periods=forecast_steps + 1, freq='D', tz='UTC')[1:]
+    plt.plot(forecast_index, forecast, label='Forecast', color='red')
+    
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title(f'ARIMA Cryptocurrency Price Forecast for {symbol}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
