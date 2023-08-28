c3b597418bbd298d5451ce48fb67f34ba2b4d59d
debydawn <debbydawn16@gmail.com>
2023-08-28 09:54:36 +0100

    Data processing

---
+++ Crypto_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+
+
+yahoo_financials = YahooFinancials('BTC-USD')
+data=yahoo_financials.get_historical_price_data("2019-07-10", "2021-05-30", "monthly")
+btc_df = pd.DataFrame(data['BTC-USD']['prices'])
+btc_df = btc_df.drop('date', axis=1).set_index('formatted_date')
+btc_df.head()
+
+
